66cea595bcc564a8c533705aaa0818cdce4d51a4
HanFaye <3294196260@qq.com>
2024-11-03 14:08:19 +0800

    1.repair pandas conflict 2.realize pygwalker:md and file

--- page4/mainpage4.py
+++ page4/mainpage4.py
@@ -1,33 +1,67 @@
 import streamlit as st
 import mdpd
+import pandas as pd
+from pygwalker.api.streamlit import StreamlitRenderer
 
 
 def mainpage4():
     st.title('绘图(markdown or 导入文件)')
+    # if st.button("清空数据"):
+    #     global df, rich_text
+    #     df = None
+    #     rich_text = rich_text_eg
     tab_md, tabdf = st.tabs(['markdown表格', '导入表格'])
     with tab_md:
         input_md()
     with tabdf:
-        input_df()
-
-
+        input_file()
+    if df is not None:
+        use_pygwalker()
+    
+def use_pygwalker():
+    st.subheader("pygwalker")
+    pyg = StreamlitRenderer(df, theme_key='streamlit')
+    pyg.explorer()
+    
+# @st.cache_resource
 def input_md():
     global rich_text, df, df_dtype
     st.subheader("markdown表格")
     st.markdown(md_tips)
     rich_text = st.text_area('输入markdown表格', value=rich_text_eg, height=200)
-    st.subheader("markdown表格预览")
-    st.markdown(rich_text)
-    st.subheader("markdown表格 > Dataframe")
-    df = mdpd.from_md(rich_text)
-    df_type, new_cols = get_df_type(df.columns.to_list())
-    df = df.astype(df_type)
-    df.columns = new_cols
+    tab_md, tab_df, tab_dtype = st.tabs(['markdown table', 'Dataframe', '数据类型'])
+    with tab_md:
+        st.markdown(rich_text)
+    with tab_df:
+        df = mdpd.from_md(rich_text)
+        df_type, new_cols = get_df_type(df.columns.to_list())
+        df = df.astype(df_type)
+        df.columns = new_cols
+        st.dataframe(df)
+        print("df change in md")
+    with tab_dtype:
+        st.write(df_type)
+
+def input_file():
+    global df
+    st.subheader("导入文件")
+    file = st.file_uploader("上传文件", type=["csv", "xlsx"])
+    if file is not None:
+        try:
+            if file.name.endswith('.csv'):
+                df = pd.read_csv(file)
+            elif file.name.endswith('.xlsx'):
+                df = pd.read_excel(file)
+            else:
+                raise ValueError('文件类型错误')
+            print("df change in file_uploader")
+        except:
+            st.error('文件读取错误')
     st.dataframe(df)
-    print(df.dtypes)
+    
+
     
 def get_df_type(col_list):
-    print(col_list)
     new_cols = []
     df_type = dict()
     for ele in col_list:
@@ -42,11 +76,8 @@ def get_df_type(col_list):
             df_type[ele] = 'float32'
         else:
             raise ValueError('类型错误')
-    print(new_cols)
     return df_type, new_cols
 
-def input_df():
-    return
 
 md_tips='''
 在md表头中注意添加数据类型
